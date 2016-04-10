d557ae18f41459037a2d30732ee705d1f919b47a
Scott <soulcramer@gmail.com>
2016-04-11 01:28:31 +0200

    final reset

--- index.js
+++ index.js
@@ -2,17 +2,6 @@
 
 let aero = require('aero')
 let app = aero()
-/*let passport = require("passport")
-let session = require("express-session")
-let fs = require('fs')
-//let apiKeys = JSON.parse(fs.readFileSync('security/api-keys.json'))
-let FacebookStrategy = require('passport-facebook').Strategy
-let facebookConfig = Object.assign({
-    callbackURL: '/auth/facebook/callback',
-    enableProof: false
-  },
-  apiKeys.facebook
-)*/
 app.run()
 
 app.get('/', (req, res) => res.redirect('/home'))
